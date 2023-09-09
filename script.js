53866e95e903db05154d4f80eaf4b297b1fe4935
mohammad sajid ali <sa4868091@gmail.com>
2023-09-09 23:18:40 +0530

    add new code

---
+++ script.js
@@ -0,0 +1,90 @@
+var icon = document.querySelector("nav h2");
+var menu = document.querySelector(".menu");
+var li_one = document.querySelector(".li-one");
+var li_two = document.querySelector(".li-two");
+var li_three = document.querySelector(".li-three");
+var li_four = document.querySelector(".li-four");
+var li_five = document.querySelector(".li-five");
+
+var flag = 0;
+
+icon.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+li_one.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+li_two.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+li_three.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+li_four.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+li_five.addEventListener("click",function(){
+    if(flag===0){
+        menu.style.top ='0'
+        icon.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag =1
+    }
+    else{
+        menu.style.top = "-100%";
+        icon.innerHTML = '<i class="ri-menu-3-line"></i>'
+        flag = 0 
+
+    }
+})
+
+
